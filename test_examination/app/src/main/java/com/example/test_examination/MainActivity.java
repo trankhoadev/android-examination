94fc2c89e866039dead437fedd699e58d70afa2b
trankhoadev <trankhoadev@gmail.com>
2022-05-08 17:18:45 +0700

    Update project

---
+++ test_examination/app/src/main/java/com/example/test_examination/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.test_examination;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.PersistableBundle;
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLogOut;
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogOut = findViewById(R.id.btnLogout);
+        mAuth = FirebaseAuth.getInstance();
+
+        btnLogOut.setOnClickListener(view ->{
+            mAuth.signOut();
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user == null){
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        }
+    }
+}
